f66d3bef7a1e23105a9773ae6dca2fef65c1eda7
Immanuella Busari <emi.busari@gmail.com>
2022-06-11 21:05:07 +0100

    Navigation

--- web-app/src/App.tsx
+++ web-app/src/App.tsx
@@ -2,19 +2,21 @@ import React from "react";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import "./App.css";
 import { MentorsPage } from "./pages/MentorsPage";
+import { Footer } from "./components/common/Footer";
+import { Header } from "./components/common/Header";
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
-        <nav>
+        {/* <nav>
           <Link to="/mentors">Mentors</Link>
-        </nav>
-        {/* Header */}
+        </nav> */}
+        <Header />
         <Routes>
           <Route path="/mentors" element={<MentorsPage />} />
         </Routes>
-        {/* Footer */}
+        <Footer />
       </div>
       ;
     </BrowserRouter>
